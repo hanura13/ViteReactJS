e120ca867a50cfd29a9fecebf4460edb8086dc13
hanura13 <hanuradiarjo@gmail.com>
2023-05-23 18:44:47 +0700

    Add Image Mission

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,5 +4,6 @@ import close from './close.svg'
 import send from './Send.svg'
 import shield from './Shield.svg'
 import star from './Star.svg'
+import mission from './mission.png'
 
-export {logo, menu, close, send, shield, star}
+export {logo, menu, close, send, shield, star, mission}
--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { mission } from '../assets'
+import style, { layout } from '../style'
 
-const Mission = () => {
-    return (
-        <div>Mission</div>
-    )
-}
+const Mission = () => (
+    <section id='mission' className={layout.sectionReverse}>
+        <div className={layout.sectionImgReverse}>
+            <img alt='mission' src={mission} className='w-[100%] h-[100%] relative z-[5]' />
+        </div>
+
+        <div className={layout.sectionInfo}>
+            <h2 className={style.heading2}>Our Mission</h2>
+            <p className={`${style.paragraph} max-w-[470px] mt-5`}>To provide accured and reliable professional services in maritime commerce, cargo operator and shipping
+                industries beside that we are committed to combine an objective and professional behavior in accordance
+                with the governing technical rules and standards, with the understanding of the commercial implications
+                of our work and in protection of our Client’s interests.</p>
+        </div>
+    </section>
+)
 
 export default Mission
